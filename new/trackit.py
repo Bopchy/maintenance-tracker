4c5c21d685a0f3a8c9f7f5e68f9f549729fd9bee
Ruth Bochere <rorbochere@gmail.com>
2016-05-30 12:02:12 +0300

    Worked on database further. Prior to implementing shell context

--- new/trackit.py
+++ new/trackit.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
-import forms
+from flask.ext.script import Shell
 from forms import SignUpForm, HomeSignUp, SignIn, RequestPost
- 
+from flask.ext.sqlalchemy import SQLAlchemy
+import os 
+import forms 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # App Initialization-- trackit is object of Flask class
 trackit = Flask(__name__)
@@ -10,7 +14,38 @@ trackit = Flask(__name__)
 # so it can find resource files relative to the location of the app
 trackit.config['SECRET_KEY'] = 'amwjenfbtvpz' 
 # Setting encryption key for CSRF
+trackit.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+		os.path.join(basedir, 'trackit_data.sqlite')
+# Creates database 'trackit_data.sqlite'
+trackit.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+trackit.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(trackit) 
+# This object instantiation of SQLAlchemy (db) represents the
+# database and is what gives access to all functionality of 
+# Flask-SQLAlchemy   
 Bootstrap(trackit)
+
+# Adding a shell context 
+# 'make_shell_context' function registers the application, db instance, 
+# models; so that they are automatically imported into the shell 
+def make_shell_context():
+	return dict(trackit=trackit, db=db, User=User, Requests=Requests)
+manager.add_command("Shell", Shell(make_context=make_shell_context))
+
+class User(db.Model):
+	__tablename__ = 'user' 
+	# Defines name of the table in the database
+	id = db.Column(db.Integer, primary_key=True) 
+	BizEmail = db.Column(db.String(100), unique=True, index=True)
+	BizName = db.Column(db.String(180), index=True)
+	Password = db.Column(db.String(15), nullable=False)
+
+class Requests(db.Model):
+	__tablename__='requests'
+	id = db.Column(db.Integer, primary_key=True)
+	Title = db.Column(db.String(120), nullable=False, index=True)
+	Comments = db.Column(db.String(1800), nullable=False)
 
 # View functions 
 @trackit.route('/', methods=['GET', 'POST'])
@@ -25,10 +60,22 @@ def index():
 	if Form.validate_on_submit():
 # Validate_on_submit() returns True if form has been submitted, and 
 # all validators were satisfied; otherwise it returns False   
-		BizEmail = Form.BizEmail.data
+		BizEmail = User.query.filter_by(BizEmail=Form.BizEmail.data)
+		# Checks to see if submitted business email already exixts in db 
+		if BizEmail is None:
+			BizEmail = Form.BizEmail.data
+			db.session.add(BizEmail) # Adds submitted email into db
+			session['exists'] = False
+			# 'exists' is a variable created to deal with the redirecting to a 
+			# page stating that email already exists in the database. 
+		else:
+			session['exists'] = True
+			#### Flash message stating email already exists in db
+		session['BizEmail'] = Form.BizEmail.data
 		Form.BizEmail.data = ''
+		return redirect(url_for('index'))
 	return render_template('home.html', Form=Form, \
-		BizEmail=BizEmail)
+		BizEmail=session.get('BizEmail')
 
 @trackit.route('/signup', methods=['GET', 'POST'])
 def register():
