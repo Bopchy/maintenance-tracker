45d768ed562b745eeb4f78df895d3b2fc87ec791
Ruth Bochere <rorbochere@gmail.com>
2016-08-05 14:54:18 +0300

    Fixed InvalidRequestError. It was just a mix up of variable names.

--- new/app/auth/views.py
+++ new/app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, url_for
+from flask import render_template, session, url_for, flash, redirect
 from flask.ext.login import login_user
 from . import auth
 from .forms import SignIn
@@ -8,17 +8,17 @@ from ..models import User
 def signin():
 	Form = SignIn()
 	if Form.validate_on_submit():
-		user = User.query.filter_by(Bizemail=Form.BizEmail.data).first()
+		user = User.query.filter_by(BizEmail=Form.BizEmail.data).first()
 		# If the form data submitted is valid, the db is queried to see
 		# if such an email as entered exists. 
 		if user is not None and user.verify_password(Form.Password.data):
-			login_user(user, remember_me.data) # Records user as logged in, in a session
+			login_user(user, Form.Remember_me.data) 
+			# Records user as logged in, in a session
 			return redirect(url_for('main.user_account'))
-		# If the email exists, password is checked to see if it matches the one in the 
-		# db. If it does, the user is logged in and proceeds to 
-		# url_for(main.user_account). 
+		# If the email exists, password is checked to see if it matches the one in the db. 
+		# If it does, the user is logged in and proceeds to url_for(main.user_account). 
 		flash('Invalid Business/Organization Email or Password.')
-		# Else, the message below is flashed.
+		# Else, the message above is flashed.
 	return render_template('auth/signin.html', Form=Form)
 	# Template stored inside templates/auth so as to avoid possible name collisions
 	# with main-blueprint related templates  that may be added in the future
